64211f2017b295628055b662de8d140a7cf59b1b
gonzalorecabal <gonzalo.recabal@gmail.com>
2025-07-03 02:36:00 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,16 +1,6 @@
 from flask import Flask, jsonify
 import requests
 from requests.auth import HTTPBasicAuth
-
-app = Flask(__name__)
-
-API_LOGIN = "4b3db3e92c23385671198d22411c1c21"
-API_TOKEN = "4e9008f506893c0d5d09a05d253294c333a90e1c8a1225e53b"
-STORE_URL = "https://technosun-cl.jumpseller.com/"
-AUTH = HTTPBasicAuth(API_LOGIN, API_TOKEN)
-from flask import Flask, jsonify
-import requests
-from requests.auth import HTTPBasicAuth
 import os
 
 app = Flask(__name__)
