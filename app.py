975e2daebda018d77f06fafee1352409616d3b5b
gonzalorecabal <gonzalo.recabal@gmail.com>
2025-07-03 03:18:18 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
 from flask import Flask, jsonify
 import requests
-from requests.auth import HTTPBasicAuth
 import os
 
 app = Flask(__name__)
 
 API_LOGIN = "4b3db3e92c23385671198d22411c1c21"
 API_TOKEN = "4e9008f506893c0d5d09a05d253294c333a90e1c8a1225e53b"
-STORE_URL = "https://api.jumpseller.com"
-AUTH = HTTPBasicAuth(API_LOGIN, API_TOKEN)
+BASE_URL = f"https://api.jumpseller.com/v1/products.json?login={API_LOGIN}&authtoken={API_TOKEN}"
 
 @app.route("/productos", methods=["GET"])
 def productos():
     try:
-        response = requests.get(f"{STORE_URL}/api/products", auth=AUTH)
+        response = requests.get(BASE_URL)
         response.raise_for_status()
         return jsonify(response.json())
     except requests.exceptions.HTTPError as e:
@@ -24,7 +22,7 @@ def productos():
 @app.route("/stock_bajo", methods=["GET"])
 def stock_bajo():
     try:
-        response = requests.get(f"{STORE_URL}/api/products", auth=AUTH)
+        response = requests.get(BASE_URL)
         response.raise_for_status()
         productos = response.json()
         bajos = [p for p in productos if int(p["product"].get("stock", 0)) < 5]
